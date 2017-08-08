b65d4d4860faaddac7859d266e3f561e698179d4
BrandtHoward <BrandtaHoward@gmail.com>
2017-08-08 12:07:51 -0400

    updatemore

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -4,4 +4,64 @@
 
 
 
+void ATankPlayerController::BeginPlay() {
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("Not Possessing"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *(ControlledTank->GetName()));
+	}
+
+}
+
+
+//Tick
+//
+//Aim toward cross hair
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	
+	Super::Tick(DeltaTime);
+	AimTowardCrosshair();
+}
+
+void ATankPlayerController::AimTowardCrosshair()
+{
+
+
+	if (!GetControlledTank()) { return; }
+	FVector HitLocation; //out parameter
+	if(GetSightRayHitLocation(HitLocation)){ 
+		//UE_LOG(LogTemp, Warning, TEXT("Hit location: %s"), *HitLocation.ToString()); 
+	}
+	
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector & OutHitLocation) const
+{
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrossHairXLocation, ViewportSizeY * CrossHairYLocation);
+
+	//UE_LOG(LogTemp, Warning, TEXT("Screen Location location: %s"), *ScreenLocation.ToString())
+	return true;
+}
+
+
+ATank*  ATankPlayerController::GetControlledTank() const {
+
+
+
+
+	return Cast<ATank>(GetPawn());
+}
+
+
+
+
+
+
+
 
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
+#include "Tank.h"
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "TankPlayerController.generated.h"
@@ -15,6 +15,26 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 	
+public:
+	ATank* GetControlledTank() const;
+
+	void BeginPlay() override;
+
+
+	virtual void Tick(float DeltaTime) override;
+
+	void AimTowardCrosshair();
 	
 	
+	
+
+private:
+	ATank* Battle;
+	bool GetSightRayHitLocation(FVector &OutHitLocation) const;
+	UPROPERTY(EditAnywhere)
+	float CrossHairXLocation = .5;
+	UPROPERTY(EditAnywhere)
+	float CrossHairYLocation = .3333;
+
+
 };
